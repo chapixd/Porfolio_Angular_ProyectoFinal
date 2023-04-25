666e2ecbfaffbbdd7cf4044dc8ffb802a5593845
maaximoarganaraz14@gmail.com <maximoarganaraz14@gmail.com>
2023-04-25 15:48:19 -0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SobreMiComponent } from './component/sobre-mi/sobre-mi.component';
+import { HabilidadesComponent } from './component/habilidades/habilidades.component';
+import { ProyectosComponent } from './component/proyectos/proyectos.component';
+import { ExperienciaLaboralComponent } from './component/experiencia-laboral/experiencia-laboral.component';
+import { FooterComponent } from './component/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SobreMiComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    ExperienciaLaboralComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,

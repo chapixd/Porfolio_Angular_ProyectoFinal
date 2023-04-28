6735f0ecb503520e885eb0084f85d406ba2cd3fc
maaximoarganaraz14@gmail.com <maximoarganaraz14@gmail.com>
2023-04-28 17:03:53 -0300

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
 import { SobreMiComponent } from './component/sobre-mi/sobre-mi.component';
 import { HabilidadesComponent } from './component/habilidades/habilidades.component';
 import { ProyectosComponent } from './component/proyectos/proyectos.component';
 import { ExperienciaLaboralComponent } from './component/experiencia-laboral/experiencia-laboral.component';
 import { FooterComponent } from './component/footer/footer.component';
-
-
+import { LogoAPComponent } from './component/logo-ap/logo-ap.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent,
     SobreMiComponent,
     HabilidadesComponent,
     ProyectosComponent,
     ExperienciaLaboralComponent,
     FooterComponent,
+    LogoAPComponent,
+    BannerComponent,
+    EducacionComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
